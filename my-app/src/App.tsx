b86a18cda614c3331adfae96ab51342f84db17f0
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-11-20 12:17:01 +0900

    헤더 css적용 중 , className이벤트 수정 필요

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,20 +1,22 @@
 // App.tsx
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 import Main from './components/layout/Main';
 import Test1 from './components/pages/test1';
 import Test2 from './components/pages/test2';
-
 function App() {
   return (
     <Router>
+      <div className='css-10qbdmk'>
       <Header />
       <Routes>
         <Route path="/alloc" element={<Main><Test1 /></Main>} />
         <Route path="/others" element={<Main><Test2 /></Main>} />
       </Routes>
       <Footer />
+      </div>
     </Router>
   );
 }
--- my-app/src/components/layout/Header.tsx
+++ my-app/src/components/layout/Header.tsx
@@ -1,34 +1,45 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../images/titleLogo.png';
 const Header = () : JSX.Element => {
-  return (<div className="header_container">
-    <div className="header_box">
+  // ul태그 class변경에 사용하기 위한 state
+  const [active_li_Class, setActive_li_Class] = useState('css-bibrta');
+  // ul태그 class변경에 사용하기 위한 함수
+  const handleNavClick = () => {
+    setActive_li_Class(active_li_Class === 'css-bibrta' ? 'css-16mln42' : 'css-bibrta');
+  };
+  return (<div className='css-crgeb6'>
+    <div className='ss-1ago99h'>
       {/* 상단 영역 */}
-      <div>
-        <div>
-          <div>제 1회 한국퀀트 챔피언십 개최합니다!!</div>
-          <div><img src="https://www.quantus.kr/static/media/bannerClick.fddcebe1e55feacf35c7ffb413be7bd2.svg" alt="link"/></div>
+      <div className='css-1e2pywg'>
+        <div className='css-h4ylub'>
+          <div className='text_wrap'>제 1회 한국퀀트 챔피언십 개최합니다!!</div>
+          <div className='img_wrap'><img src="https://www.quantus.kr/static/media/bannerClick.fddcebe1e55feacf35c7ffb413be7bd2.svg" alt="link"/></div>
         </div>
       </div>
       {/* 중단 영역 */}
-      <div>
-        <div>
-          <img src={Logo} alt="" style={{width : '50px', height : '50px'}}/>
-          <h2>광고 창</h2>
-          <div>
-            <div>
-              <div>고급</div>
-              <div>개발자님 환영합니다!</div>
+      <div className='css-j1yknm'>
+        <div className='css-jhvj32'>
+          <img className='logo' src={Logo} alt="" style={{width : '191px', height : '37px'}}/>
+          <h2>풀스택 개발자 장루빈</h2>
+          <div className='css-1gf2x3a'>
+            <div className='css-ov1ktg'>
+              <div className='css-kenvdg'>
+                <div className='easy'>초급</div>
+                <div className='intermediate'>중급</div>
+                <div className='advanced'>고급</div>
+              </div>
+                <p>로그인 하러가기</p>
             </div>
           </div>
         </div>
       </div>
       {/* 하단 영역 */}
-      <div>
-        <div>
-          <nav>
-            <ul>
-              <li>
+      <div className='css-d2epat'>
+        <div className='css-ocv3tk'>
+          <nav className='css-18sjzx1'>
+            <ul className='css-h57cwe'>
+              <li className={active_li_Class} onClick={handleNavClick}>
               <Link to="/others">백 테스트</Link>
               </li>
               <li>
@@ -52,7 +63,7 @@ const Header = () : JSX.Element => {
             </ul>
           </nav>
         </div>
-        <div></div>
+        <div className='css-v4mbwx'></div>
       </div>
     </div>
   </div>)
