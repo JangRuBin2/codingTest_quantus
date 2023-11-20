ada8f1a8e02710623daa67d54e568d8550134c94
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-11-20 19:19:28 +0900

    헤더, 메인, 푸터 모두 제작완료 -> 여기서 부터는 '자산 분배' 페이지만 제작하겠음

--- my-app/src/components/layout/Footer.tsx
+++ my-app/src/components/layout/Footer.tsx
@@ -1,6 +1,21 @@
+import '../../App.css';
+import title from '../images/title2.png';
 const Footer = () : JSX.Element => {
-  return (<div>
-    푸터
+  return (
+  <div className='css-1c6t4tg'>
+    <div className='css-v4mbwx'>
+      <div className='css-1yzjtqd'>
+          <div>
+            <img src={title} alt="logo" style={{width :'191px', height :'37px'}}/>
+            <p className='title'>주식회사 퀀터스테크놀로지스</p>
+            <p>지원자 명 : 장루빈 | 열정 넘치는 신입 개발자</p>
+            <a href='https://github.com/JangRuBin2'>github 바로 가기</a>
+            <p>e-mail : wkdfnqls2465@gmail.com</p>
+            <p className='right_text'>오늘도 좋은 하루 되세요~ 2024년도 화이팅</p>
+          </div>
+          <div className='css-7360bc'></div>
+      </div>
+    </div>
   </div>)
 }
 export default Footer;
--- my-app/src/components/layout/Main.tsx
+++ my-app/src/components/layout/Main.tsx
@@ -6,7 +6,18 @@ interface MainProps {
 }
 
 const Main: React.FC<MainProps> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+  <div className='css-ov1ktg'>
+    <div className='css-boa882'></div>
+    <div className='css-1v1xjc8'>
+      <div className='css-10000pf'>
+        <div className='css-1hkbw9e'></div>
+      </div>
+      {children}
+    </div>
+    <div className='css-boa882'></div>
+  </div>
+    );
 }
 
 export default Main;
